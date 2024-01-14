5570041978188e6aa587c56fc0aa930759ff83fc
TimCauss <t.caussignac@gmail.com>
2024-01-14 12:10:10 +0100

    doc: adding docstrings to the logger object

--- Libs/logger.py
+++ Libs/logger.py
@@ -1,12 +1,36 @@
 import logging
 
 class Logger:
-    def __init__(self, name, file="app.log") -> None:
+    """
+        A simple logger class for logging messages to a file.
+        
+        Attributes:
+        logger (Logger): An instance of the logging.Logger class.
+    """
+    def __init__(self, name:str, file="app.log") -> None:
+        """
+        The constructor for the Logger class.
+        
+        Parameters:
+            name (str): The name of the logger.
+            file (str): The name of the file where logs will be written.
+        """
         self.logger = logging.getLogger(name)
         self.logger.setLevel(logging.INFO)
         handler = logging.FileHandler(file)
         handler.setFormatter(logging.Formatter('%(asctime)s %(message)s'))
         self.logger.addHandler(handler)
         
-    def log(self, message):
+    def log(self, message:str):
+        """
+        Logs a message to the file specified in the constructor.
+
+        Parameters:
+            message (str): The message to be logged.
+        """
         self.logger.info(message)
+        
+
+# Utilisation:
+# logger = Logger('my_logger')
+# logger.log('This is a log message')
