9a6dd8df6874df9e7712d5801cf97f6e2d6f16ef
TimCauss <t.caussignac@gmail.com>
2024-01-14 12:14:19 +0100

    feats: adding middleware, Adding logs

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from Libs.logger import Logger
+import time
+import logging
+import uuid
+
 # from models import root_model
 
+logger = Logger("req_logger")
+
 app = FastAPI()
+
+@app.middleware("http")
+async def logs_requests(request: Request, call_next):
+    #Adding ID for each request and IP source
+    request_id = str(uuid.uuid4())
+    source_ip = request.client.host
+    start_time = time.time()
+    
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    
+    #logging informations
+    logger.log(f'Request ID : {request_id} | Source IP : {source_ip} | Method: {request.method} | URL: {request.url} | Processing Time: {process_time} seconds.')
+    
+    return response
 
 
 @app.get("/")
 async def index() -> bool:
     return True
-
-
-@app.get('/creds')
-async def read_creds():
-    pass
