23c6ae1b3d48650dae0579da9361340b4eac76a2
TimCauss <t.caussignac@gmail.com>
2024-01-16 10:37:44 +0100

    debug logging

--- main.py
+++ main.py
@@ -15,14 +15,11 @@ async def logs_requests(request: Request, call_next):
     #Adding ID for each request and IP source
     request_id = str(uuid.uuid4())
     source_ip = request.client.host
-    start_time = time.time()
     
-    response = await call_next(request)
-    process_time = time.time() - start_time
-    
-    #logging informations
     logger.log(f'Request ID : {request_id} | Source IP : {source_ip} | Method: {request.method} | URL: {request.url} | Processing Time: {process_time} seconds.')
-    
+
+    response = await call_next(request)
+
     return response
 
 
