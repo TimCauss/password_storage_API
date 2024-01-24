5388bc8219745393ad459155b26545d6087fa892
TimCauss <t.caussignac@gmail.com>
2024-01-24 03:04:22 +0100

    Feat: adding requests process time to logger in the middleware

--- main.py
+++ main.py
@@ -4,25 +4,26 @@ import time
 import logging
 import uuid
 
-# from models import root_model
 
 logger = Logger("req_logger")
-
 app = FastAPI()
 
 @app.middleware("http")
 async def logs_requests(request: Request, call_next):
     #Adding ID for each request and IP source
+    start_time = time.time()
     request_id = str(uuid.uuid4())
     source_ip = request.client.host
     
-    logger.log(f'Request ID : {request_id} | Source IP : {source_ip} | Method: {request.method} | URL: {request.url} | Processing Time: {process_time} seconds.')
-
     response = await call_next(request)
+    process_time = time.time() - start_time
+    
+    logger.log(f'Request ID: {request_id} | Source IP : {source_ip} | Method: {request.method} | URL: {request.url} | time: {process_time}')
 
     return response
 
 
 @app.get("/")
 async def index() -> bool:
+    time.sleep(5)
     return True
